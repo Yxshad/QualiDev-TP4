433b79b62e6fd5c129314ba32f36e3a25c478b25
Yxshad <elavergne001@iutbayonne.univ-pau.fr>
2024-10-14 10:35:26 +0200

    Modification static final

--- src/main/java/TennisSonarLint.java
+++ src/main/java/TennisSonarLint.java
@@ -7,6 +7,10 @@ public class TennisSonarLint implements TennisGame
     private String p1Res = "";
     private String p2Res = "";
 
+    public static final String FORTY = "Forty";
+    public static final String FIFTEEN = "Fifteen";
+    public static final String THIRTY = "Thirty";
+
     public TennisSonarLint(String player1Name, String player2Name) {
     }
 
@@ -16,9 +20,9 @@ public class TennisSonarLint implements TennisGame
             if (p1Point == 0)
                 score = "Love";
             if (p1Point == 1)
-                score = "Fifteen";
+                score = FIFTEEN;
             if (p1Point == 2)
-                score = "Thirty";
+                score = THIRTY;
             score += "-All";
         }
         if (p1Point == p2Point && p1Point >= 3)
@@ -30,11 +34,11 @@ public class TennisSonarLint implements TennisGame
         String score = "";
         if (p1Point > 0 && p2Point == 0) {
             if (p1Point == 1)
-                p1Res = "Fifteen";
+                p1Res = FIFTEEN;
             if (p1Point == 2)
-                p1Res = "Thirty";
+                p1Res = THIRTY;
             if (p1Point == 3)
-                p1Res = "Forty";
+                p1Res = FORTY;
 
             p2Res = "Love";
             score = p1Res + "-" + p2Res;
@@ -46,11 +50,11 @@ public class TennisSonarLint implements TennisGame
         String score = "";
         if (p2Point > 0 && p1Point == 0) {
             if (p2Point == 1)
-                p2Res = "Fifteen";
+                p2Res = FIFTEEN;
             if (p2Point == 2)
-                p2Res = "Thirty";
+                p2Res = THIRTY;
             if (p2Point == 3)
-                p2Res = "Forty";
+                p2Res = FORTY;
 
             p1Res = "Love";
             score = p1Res + "-" + p2Res;
@@ -61,13 +65,13 @@ public class TennisSonarLint implements TennisGame
         String score = "";
         if (p1Point > p2Point && p1Point < 4) {
             if (p1Point == 2)
-                p1Res = "Thirty";
+                p1Res = THIRTY;
             if (p1Point == 3)
-                p1Res = "Forty";
+                p1Res = FORTY;
             if (p2Point == 1)
-                p2Res = "Fifteen";
+                p2Res = FIFTEEN;
             if (p2Point == 2)
-                p2Res = "Thirty";
+                p2Res = THIRTY;
             score = p1Res + "-" + p2Res;
         }
         return score;
@@ -78,13 +82,13 @@ public class TennisSonarLint implements TennisGame
         if (p2Point > p1Point && p2Point < 4)
         {
             if (p2Point ==2)
-                p2Res ="Thirty";
+                p2Res =THIRTY;
             if (p2Point ==3)
-                p2Res ="Forty";
+                p2Res = FORTY;
             if (p1Point ==1)
-                p1Res ="Fifteen";
+                p1Res =FIFTEEN;
             if (p1Point ==2)
-                p1Res ="Thirty";
+                p1Res =THIRTY;
             score = p1Res + "-" + p2Res;
         }
         return score;}
